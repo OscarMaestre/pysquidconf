ee75e6809576ecad2997c664b6b2bfa44f80e914
Tu Nombre <you@example.com>
2023-02-28 22:42:54 +0100

    Added certificate data in command line

--- create_simple_conf_file.py
+++ create_simple_conf_file.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 from pysquidconf.conf import ConfFile
-from pysquidconf.commands import Squid
+from pysquidconf.commands import Squid, CertificateData
+
+
+def get_certificate_data():
+    certificate_data=CertificateData("ES", "CLM", "CR", "IES Maestre de Calatrava","Dep Informatica", "Profesor", "nada@gmail.com")
+    print(certificate_data)
+    return certificate_data
 
 def crear_fichero():
     filename="mifichero.conf"
-    mb_cache_size=50
+    mb_cache_size=75
     certificate_file_for_browser="FirefoxCertificate.crt"
     private_key_file="PrivateKey.key"
+    
     conf=ConfFile()
     conf.add_network_acl("Oficina", "172.16.0.0/16", allow=False, comment="Denegar 172.16.0.0/16")
     conf.add_network_acl("Oficina", "192.168.0.0/24", allow=True, comment="Permitir 192.168.0.0/24")
@@ -19,14 +26,16 @@ def crear_fichero():
     conf.add_url_regex("paginas_violentas", "viol", allow=False, comment="")
 
     conf.add_ssl_start("manipular_certificados", comment="Procesado de certificados")
-    conf.add_ssl_end("/etc/squid/ficheros", 50,comment="Generacion de certificados")
+    conf.add_ssl_end("/etc/squid/ficheros", mb_cache_size,comment="Generacion de certificados")
     conf.add_http_port(mb_cache_size,certificate_file_for_browser,private_key_file)
     conf.save(filename)
 
+    certificate=get_certificate_data()
     squid_commands=Squid(private_key_file, certificate_file_for_browser,
                          validity_in_days=365, rsa_key_length=2048,
                          ssl_certs_directory="/etc/squid/certificados_squid_ssl",
-                         comment_http_port=True, conf_file=filename)
+                         comment_http_port=True, conf_file=filename, ssl_store_mb_limit_size=mb_cache_size,
+                         certificate_data=certificate)
     squid_commands.save("squid.sh")
 
 if __name__=="__main__":
--- pysquidconf/commands.py
+++ pysquidconf/commands.py
@@ -1,21 +1,40 @@
+from dataclasses import dataclass
 
+@dataclass
+class CertificateData(object):
+    country_name:str
+    state_name:str
+    locality_name:str
+    organization_name:str
+    organizational_unit:str
+    common_name:str
+    email:str
+    def __str__(self) -> str:
+        text=f'/C=<{self.country_name}>/ST=<{self.state_name}>/L=<{self.locality_name}>/O=<{self.organization_name}>/CN=<{self.common_name}>/OU=<{self.organizational_unit}>'
+        return text
 
 class Squid(object):
-    def __init__(self, private_key_file, self_signed_certificate_file, rsa_key_length, validity_in_days, ssl_certs_directory, user_that_squid_uses="proxy", comment_http_port=False, conf_file="/etc/squid/conf.d/generated_file.conf") -> None:
-        
+    def __init__(self, private_key_file, self_signed_certificate_file, rsa_key_length, validity_in_days, ssl_certs_directory, user_that_squid_uses="proxy", comment_http_port=False, conf_file="/etc/squid/conf.d/generated_file.conf", ssl_store_mb_limit_size=50, certificate_data:CertificateData=None) -> None:
+        if certificate_data==None:
+            raise Exception("Please provide certificate data")
+        str_certificate=str(certificate_data)
+
+        self.stop_service           = "sudo service squid stop"
         self.remove_squid           = "sudo apt-get purge -y squid-openssl"
         self.remove_confs           = "sudo rm -rf /etc/squid"
         self.install_squid          = "sudo apt-get install -y squid-openssl"
         self.copy_conf_file         = f'sudo cp {conf_file} /etc/squid/conf.d'
-        self.certificate_generation = f'sudo openssl req -new -newkey rsa:{rsa_key_length} -days {validity_in_days} -nodes -x509 -keyout {private_key_file} -out {self_signed_certificate_file}'
-        self.assign_permission      ="sudo chown {0}:{0} {1}".format(user_that_squid_uses, ssl_certs_directory)
-        self.comment_http_port      =comment_http_port
+        self.certificate_generation = f'sudo openssl req -new -newkey rsa:{rsa_key_length} -days {validity_in_days} -nodes -x509 -keyout {private_key_file} -out {self_signed_certificate_file} -subj \"{str_certificate}\"'
+        self.create_ssl_store       = f'sudo /usr/lib/squid/security_file_certgen  -c -s {ssl_certs_directory} -M {ssl_store_mb_limit_size}MB'
+        self.assign_permission      = "sudo chown {0}:{0} {1}".format(user_that_squid_uses, ssl_certs_directory)
+        self.comment_http_port      = comment_http_port
         
     def get_text(self):
         commands=[
             self.remove_squid,
             self.install_squid,
             self.certificate_generation,
+            self.create_ssl_store,
             self.assign_permission
         ]
         if self.comment_http_port:
